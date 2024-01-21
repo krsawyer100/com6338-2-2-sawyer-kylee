24119017a4dbe07991064377d8df3aaa4700b6bd
Kylee Sawyer <k.sawyer@ufl.edu>
2024-01-20 19:14:47 -0500

    JavaScript Added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function greet() {
+    var userName = prompt('What is your name?');
+    alert('Hello, ' + userName + '!');
+    var ageEntered = prompt('How old are you?');
+    var userAge = parseInt(ageEntered);
+    var birthday = confirm('Have you had a birthday yet this year?');
+    if (birthday === true) {
+        alert('You were born in ' + (new Date().getFullYear() - userAge) + '!');
+    } else
+        alert('You were born in ' + (new Date().getFullYear() - (userAge + 1)) + '!');
+}
